c2a387688f1fd40de5da57d8f40a2d9ecb06d546
Eduardo Aguilar Yépez <dante.aguilar41@gmail.com>
2016-06-16 12:21:14 -0500

    change log

---
+++ CHANGELOG.md
@@ -0,0 +1,6 @@
+## 10/06/2016 v1.1.0
+* Feature: Seleccion de proveedores
+* Feature: Normalizacion de vista de seleccion de proveedores
+* Feature: Validacion de proveedor obligatorio
+* Feature: Captura de telefono de cliente
+* Remove: Banner en seleccion de tiendas
--- app/code/community/Compropago/Block/OnepageSuccess.php
+++ app/code/community/Compropago/Block/OnepageSuccess.php
@@ -1,13 +1,16 @@
-
 <?php
-	class Compropago_Block_OnepageSuccess extends Mage_Checkout_Block_Onepage_Success
-	{
-	// Write your custom methods
-	// All parent’s methods also will work
-		protected function _beforeToHtml() 
-	    {	    	
-	        $outHtml = parent::_beforeToHtml();
-	        $this->setTemplate('compropago/onepage_success.phtml');
-	        return $outHtml;
-	    }
-	}
+
+class Compropago_Block_OnepageSuccess extends Mage_Checkout_Block_Onepage_Success
+{
+    /**
+     * Regresa el recibo de compra
+     *
+     * @return mixed 
+     */
+    protected function _beforeToHtml()
+    {
+        $outHtml = parent::_beforeToHtml();
+        $this->setTemplate('compropago/onepage_success.phtml');
+        return $outHtml;
+    }
+}
